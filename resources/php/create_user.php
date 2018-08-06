00032a21a3e681022407cc8482e230375eba9120
team9hacker <team9hacker@yandex.com>
2018-08-06 17:42:55 -0500

    Added notes and guide for aws db connect

--- resources/php/create_user.php
+++ resources/php/create_user.php
@@ -6,6 +6,33 @@ function createUser($USER, $EMAIL, $PASSWD) {
          "Password: " . $PASSWD . "<p>";
 }
 
+// CHEAT sheet
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create_deploy_PHP.rds.html#php-rds-create
+//
+// will be using -->  SQL Server – PDO_SQLSRV
+    //
+    //
+    // RDS_HOSTNAME – The hostname of the DB instance.
+    //
+    // Amazon RDS console label – Endpoint (this is the hostname)
+    //
+    // RDS_PORT – The port on which the DB instance accepts connections. The default value varies among DB engines.
+    //
+    // Amazon RDS console label – Port
+    //
+    // RDS_DB_NAME – The database name, ebdb.
+    //
+    // Amazon RDS console label – DB Name
+    //
+    // RDS_USERNAME – The user name that you configured for your database.
+    //
+    // Amazon RDS console label – Username
+    //
+    // RDS_PASSWORD – The password that you configured for your database.
+
+//
+
+
 // Determin action
 if($_SERVER['REQUEST_METHOD'] == "POST") {
     if( isset($_POST["userName"], $_POST["emailName"], $_POST["passwdVal"])) {
