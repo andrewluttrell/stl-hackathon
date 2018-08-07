b04e6f8186d5f40c64d720be4176568803bee401
team9hacker <team9hacker@yandex.com>
2018-08-07 14:33:44 -0500

    update

--- resources/php/user_home.php
+++ resources/php/user_home.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <title>User Home</title>
+        <title>#IdoS Login</title>
+        <link rel="stylesheet" href="../css/base.css"/>
+        <link rel="stylesheet" href="../css/main.css"/>
     </head>
     <body>
 
@@ -11,7 +14,13 @@
             echo "<center><h2>Not logged in!</h2></center>";
             echo "<center><h3>PLease login <a href='../../login.html'>HERE</a></h3></center>";
         } else {
-            echo "Logged in!!";
+            echo "<div id='master-container' class='background-gradient-purple'>";
+            echo "<img clas='owlSmall' src='../images/owl.svg'/>";
+            echo "<center><p>Choose what you want to do...</p></center>";
+            echo "<div class='userHomeRow1'><img clas='owlSmall' src='../images/cam.svg'/><span>Let's go shopping!</span></div>";
+            echo "<div class='userHomeRow2'><img clas='owlSmall' src='../images/glass.svg'/><span>Search for your goods.</span></div>";
+            echo "<div class='userHomeRow3'><img clas='owlSmall' src='../images/book.svg'/><span>Definitions for all those strange terms.</span></div>" ;
+            echo "</div>";
         }
     ?>
 
