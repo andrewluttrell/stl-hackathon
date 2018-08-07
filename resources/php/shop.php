44b751adbcd8e6b7b6af2e6fd3cd6ed784f2f296
Andrew Luttrell <luttrell.andrew@gmail.com>
2018-08-07 17:07:39 -0500

    remove mockup and add back in the shop page

---
+++ resources/php/shop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Let's go Shopping! #iDOS</title>
+    <link rel="stylesheet" href="../css/base.css"/>
+    <link rel="stylesheet" href="../css/main.css"/>
+  </head>
+  <body>
+    <div id='master-container' class='background-gradient-purple'>
+      <div class="ax_default button">
+        <video src="../images/u276.png"><span>Back</span>
+      </div>
+      <img class="img " id="u264_img" src="../images/u264.png">
+    </div>
+  </body>
+</html>
