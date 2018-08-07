fe510226f7fad18d44cf2f4027e35db34cc68afe
Andrew Luttrell <luttrell.andrew@gmail.com>
2018-08-07 15:19:12 -0500

    login working

--- resources/php/signup.php
+++ resources/php/signup.php
@@ -1,18 +1,5 @@
 <?php
 include("db.php");
-include("user.php");
-// class Shop_user {
-//   function Shop_user($user, $pass) {
-//     $this->name = $user;
-//     $this->pass = $pass;
-//     $this->interest_rate = 16.99;
-//     $this->min_monthly = 30;
-//     $this->loan_rate = 5;
-//     $this->lawn = 25;
-//     $this->babysit = 15;
-//     $this->payoff = 6;
-//   }
-// }
 function createUser($USER, $PASSWD) {
     $user = getNewUser();
     $user->name = $USER;
