a6acc1d488c19fedf3551f9ece94510d90f82b18
Andrew Luttrell <luttrell.andrew@gmail.com>
2018-08-07 15:34:28 -0500

    Create user makes a cookie and redirects

--- resources/php/signup.php
+++ resources/php/signup.php
@@ -1,11 +1,22 @@
 <?php
+// Inialize session
+session_start();
+
 include("db.php");
+
 function createUser($USER, $PASSWD) {
     $user = getNewUser();
     $user->name = $USER;
     $user->pass = $PASSWD;
 
     insertNewUser($user);
+
+    $cookie_name = "userDat";
+    $cookie_value = $USER;
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+    header("Location:user_home.php");
+    exit();
 }
 
 function console_log( $data ){
@@ -20,6 +31,7 @@ $passwd="";
 if($_SERVER['REQUEST_METHOD'] == "POST") {
     if( isset($_POST["userLogin"], $_POST["userPasswd"])) {
       createUser($_POST["userLogin"], $_POST["userPasswd"]);
+
     } else {
         echo "<h2>Error! Illegal Access Attempt!</h2>";
     }
