97b15cff382f1cbe54a96357a2265e895dc98a18
Andrew Luttrell <luttrell.andrew@gmail.com>
2018-08-08 11:24:31 -0500

    Changes to payment options page

--- resources/php/payment_options.php
+++ resources/php/payment_options.php
@@ -9,6 +9,7 @@ $loanInterest = 68.24;
 $totalCost_cash = $cost + $salesTax;
 $totalCost_cc =  $cost + $salesTax + $ccInterest;
 $totalCost_loan = $cost + $salesTax + $loanInterest;
+$paymentTypes = array( "Cash", "Credit Card", "Loan" );
 ?>
 <html>
   <head>
@@ -41,7 +42,7 @@ $totalCost_loan = $cost + $salesTax + $loanInterest;
       <td>$<?= $ccInterest ?></td>
       <td>$<?= $loanInterest ?></td>
     </tr>
-    <tr>
+    <tr class="totalRow">
       <td>Total Cost</td>
       <td>$<?= $totalCost_cash ?></td>
       <td>$<?= $totalCost_cc ?></td>
@@ -54,4 +55,23 @@ $totalCost_loan = $cost + $salesTax + $loanInterest;
       <td>31 month loan @ $30.00 per month. Annual interest rate of <?= $loanRate ?>%.</td>
     </tr>
   </table>
+
+<?php foreach ( $paymentTypes as $paymentType ) { ?>
+  <div style="width:414px;">
+    <h2 style="color:white; background-color: rgba(17, 118, 181, 1); height: 30px; padding-top: 10px;"><?= $paymentType ?> Option</h2>
+    <div>
+      Cost of Item: $<?= $cost ?><br />
+      Sales Tax: $<?= $salesTax ?><br />
+      <?php if( $paymentType == "Credit Card" ) { ?>
+      Interest: $<?= $ccInterest ?><br />
+      Total Cost: $<?= $totalCost_cc ?><br />
+      <?php } else if ( $paymentType == "Loan" ) { ?>
+      Interest: $<?= $loanInterest ?><br />
+      Total Cost: $<?= $totalCost_loan ?><br />
+      <?php } else { ?>
+      Total Cost: $<?= $totalCost_cash ?><br />
+      <?php } ?>
+    </div>
+  </div>
+<?php } ?>
 </body>
